c0e947ab3b292432e2687b111d0d550ecd99b3a5
shaqna <shaquillerizkirn@gmail.com>
2023-07-24 15:24:42 +0700

    add new activity: FilterActivity

---
+++ gateway/android/BeaconIOTGateway/app/src/main/java/com/maou/beaconiotgateway/presentation/filter/FilterActivity.kt
@@ -0,0 +1,12 @@
+package com.maou.beaconiotgateway.presentation.filter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.maou.beaconiotgateway.R
+
+class FilterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_filter)
+    }
+}
