9e14b8ab856b8b3f512087eeeb35f4f34ff5ad73
shaqna <shaquillerizkirn@gmail.com>
2023-07-24 16:08:34 +0700

    modified dataModule

--- gateway/android/BeaconIOTGateway/app/src/main/java/com/maou/beaconiotgateway/di/DataModule.kt
+++ gateway/android/BeaconIOTGateway/app/src/main/java/com/maou/beaconiotgateway/di/DataModule.kt
@@ -2,8 +2,10 @@ package com.maou.beaconiotgateway.di
 
 import com.maou.beaconiotgateway.data.controller.BluetoothLeControllerImpl
 import com.maou.beaconiotgateway.data.repository.BeaconRepositoryImpl
+import com.maou.beaconiotgateway.data.repository.BusRepositoryImpl
 import com.maou.beaconiotgateway.domain.controller.BluetoothLeController
 import com.maou.beaconiotgateway.domain.repository.BeaconRepository
+import com.maou.beaconiotgateway.domain.repository.BusRepository
 import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
@@ -15,6 +17,10 @@ val dataModule = module {
 
     singleOf(::BeaconRepositoryImpl) {
         bind<BeaconRepository>()
+    }
+
+    singleOf(::BusRepositoryImpl) {
+        bind<BusRepository>()
     }
 }
 
