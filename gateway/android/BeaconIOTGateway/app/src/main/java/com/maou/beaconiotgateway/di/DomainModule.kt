8c5d25137e4f6b0f9a0a6d2d5215639ea9874803
shaqna <shaquillerizkirn@gmail.com>
2023-07-24 16:18:28 +0700

    modified domain module

--- gateway/android/BeaconIOTGateway/app/src/main/java/com/maou/beaconiotgateway/di/DomainModule.kt
+++ gateway/android/BeaconIOTGateway/app/src/main/java/com/maou/beaconiotgateway/di/DomainModule.kt
@@ -2,6 +2,8 @@ package com.maou.beaconiotgateway.di
 
 import com.maou.beaconiotgateway.domain.usecase.BeaconInteractor
 import com.maou.beaconiotgateway.domain.usecase.BeaconUseCase
+import com.maou.beaconiotgateway.domain.usecase.bus.BusInteractor
+import com.maou.beaconiotgateway.domain.usecase.bus.BusUseCase
 import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
@@ -9,5 +11,9 @@ import org.koin.dsl.module
 val domainModule = module {
     singleOf(::BeaconInteractor) {
         bind<BeaconUseCase>()
+    }
+
+    singleOf(::BusInteractor) {
+        bind<BusUseCase>()
     }
 }
