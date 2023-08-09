f47a761f21ae69a33fc1d5d1f558663e710850b4
shaqna <shaquillerizkirn@gmail.com>
2023-08-09 13:16:42 +0700

    move the contents of the bluetooth package to the scan package

---
+++ gateway/android/BeaconIOTGateway/app/src/main/java/com/maou/beaconiotgateway/presentation/scan/BleAdapter.kt
@@ -0,0 +1,53 @@
+package com.maou.beaconiotgateway.presentation.scan
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.maou.beaconiotgateway.databinding.ItemDeviceBinding
+import com.maou.beaconiotgateway.domain.model.BleDevice
+import com.maou.beaconiotgateway.utils.TimeHelper
+
+class BleAdapter : RecyclerView.Adapter<BleAdapter.ViewHolder>() {
+    private val listItem = arrayListOf<BleDevice>()
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun setItemList(items: List<BleDevice>) {
+        listItem.clear()
+        listItem.addAll(items)
+        notifyDataSetChanged()
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun clear() {
+        listItem.clear()
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val itemBinding =
+            ItemDeviceBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(itemBinding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(listItem[position])
+    }
+
+    override fun getItemCount(): Int {
+        return listItem.size
+    }
+
+    inner class ViewHolder(private val binding: ItemDeviceBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+            fun bind(items: BleDevice) {
+                with(binding) {
+                    tvDeviceName.text = items.deviceName
+                    tvDeviceAddress.text = items.deviceAddress
+                    tvDeviceRssi.text = "${items.rssi} dBm"
+                    tvDeviceDate.text = TimeHelper.timestampToDate(items.timestamp)
+                }
+            }
+    }
+}
---
+++ gateway/android/BeaconIOTGateway/app/src/main/java/com/maou/beaconiotgateway/presentation/scan/MainViewModel.kt
@@ -0,0 +1,93 @@
+package com.maou.beaconiotgateway.presentation.scan
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.maou.beaconiotgateway.data.BaseResult
+import com.maou.beaconiotgateway.domain.controller.BluetoothLeController
+import com.maou.beaconiotgateway.domain.model.BleDevice
+import com.maou.beaconiotgateway.domain.model.Bus
+import com.maou.beaconiotgateway.domain.model.BusStop
+import com.maou.beaconiotgateway.domain.usecase.BeaconUseCase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import org.koin.dsl.module
+import org.koin.androidx.viewmodel.dsl.viewModelOf
+
+class MainViewModel(
+    private val bluetoothLeController: BluetoothLeController,
+    private val beaconUseCase: BeaconUseCase
+) : ViewModel() {
+
+    var bus: List<Bus> = arrayListOf()
+    var busStop: List<BusStop> = arrayListOf()
+
+    private val _state = MutableStateFlow(BluetoothLeUiState())
+    val state = combine(
+        bluetoothLeController.scannedDevice,
+        _state
+    ) { scannedDevice, state ->
+        state.copy(scannedDevices = scannedDevice)
+    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), _state.value)
+
+    private val _beaconState = MutableStateFlow<BeaconUiState>(BeaconUiState.InitBeaconState)
+
+    val beaconState get() = _beaconState
+
+    private var _distanceScanning: Int = 0
+    val distanceScanning get() = _distanceScanning
+
+    fun setBusTarget(busTarget: List<Bus>) {
+        bus = busTarget
+    }
+
+    fun setBusStopTarget(busStopTarget: List<BusStop>) {
+        busStop = busStopTarget
+    }
+
+    fun startLeScanning() {
+        _distanceScanning += 1
+        bus.let { bluetoothLeController.startLeScanning(it) }
+    }
+
+    fun stopLeScanning() {
+        bluetoothLeController.stopLeScanning()
+    }
+
+    fun sendBeaconData(bleDevice: BleDevice) {
+
+        val busStopActive = busStop.find {
+            it.active
+        }
+
+        viewModelScope.launch {
+            beaconUseCase.sendBeaconData(bleDevice, busStopActive?.id!!.toInt()).collect { result ->
+                when (result) {
+                    is BaseResult.Error -> _beaconState.value =
+                        BeaconUiState.OnErrorAction(result.message)
+
+                    is BaseResult.Success -> _beaconState.value =
+                        BeaconUiState.OnSuccessAction(result.data)
+                }
+            }
+        }
+    }
+
+    companion object {
+        fun inject() = module {
+            viewModelOf(::MainViewModel)
+        }
+    }
+}
+
+data class BluetoothLeUiState(
+    val scannedDevices: List<BleDevice> = emptyList()
+)
+
+sealed class BeaconUiState {
+    object InitBeaconState : BeaconUiState()
+    data class OnSuccessAction(val message: String) : BeaconUiState()
+    data class OnErrorAction(val message: String) : BeaconUiState()
+}
